215d597fa17da9470c182fa033be7b8bb88273ff
Ignacio Ferrante <ignacioferrante1379@gmail.com>
2020-10-01 12:37:57 -0300

    Lo subo funcionando, deberia estar bien

--- scanner.c
+++ scanner.c
@@ -1,14 +1,17 @@
 #include "scanner.h"
 
-static int tabla[8][6] = {
-    { 1 , 2 , 3 , 4 , 5 , 0},
-    { 1 , 6 , 6 , 6 , 6 , 6},
-    { 2 , 2 , 7 , 7 , 7 , 7},
-    { 8 , 8 , 3 , 8 , 8 , 8},
-    {99 ,99 ,99 ,99 ,99 ,99},
-    {99 ,99 ,99 ,99 ,99 ,99},
-    {99 ,99 ,99 ,99 ,99 ,99},
-    {99 ,99 ,99 ,99 ,99 ,99},
+static int tabla[9][6] = {
+  //{ 0 , 1 , 2 , 3 , 4 , 5},
+  //{DIGITO, LETRA, OTRO, NUMERAL, FNC, ESPACIO}
+    { 1 , 2 , 3 , 4 , 5 , 0},   // inicial
+    { 1 , 6 , 6 , 6 , 6 , 6},   // Reconoce y almacena una constante entera
+    { 2 , 2 , 7 , 7 , 7 , 7},   // Reconoce y almacena un identificador
+    { 8 , 8 , 3 , 8 , 8 , 8},   // Reconoce y almacena un error
+    {99 ,99 ,99 ,99 ,99 ,99},   // Reconoce un numeral
+    {99 ,99 ,99 ,99 ,99 ,99},   // Se terminó la cadena
+    {99 ,99 ,99 ,99 ,99 ,99},   // Se reconoció la constante entera
+    {99 ,99 ,99 ,99 ,99 ,99},   // Se reconoció un identificador
+    {99 ,99 ,99 ,99 ,99 ,99},   // Error léxico
     };
 
 // ------------------------------------------------------------------------------------------
@@ -38,7 +41,6 @@ int aceptarToken(char caracter, int estadoActual)
   switch(estadoActual)
 	{
     case 4:
-        ungetc(caracter,stdin);
 		    return NUMERAL;
     
     case 5:
@@ -61,7 +63,7 @@ int aceptarToken(char caracter, int estadoActual)
 int scanner()
 {
   int token;
-  int estadoActual;
+  int estadoActual = 0;
   char caracter;
 
 	do
@@ -72,10 +74,16 @@ int scanner()
 	}while(estadoActual < 4);
 
   token = aceptarToken(caracter, estadoActual);
-  estadoActual = 0;
 	return token;
 }
 
 /*
+-----ERRORES-----
+
+Si recibo un # debo ir directo a un estado final aceptor sin pasar por estados intermedios.
 En el código está emparchado porque hacen ungetc cuando es numeral, pero resulta que numeral no lleva centinela por lo tanto no debe tener ungetc
+En cambiarEstado porque usaron el número mágico 35 en lugar de '#' que es muchísimo más claro?
+La última sentencia de scanner estadoActual = 0; no tiene sentido y además scanner devuelve token y no hay ningún return.
+No tiene ningún sentido que estadoActual sea una variable global, debe ser local de scanner
+
 */
